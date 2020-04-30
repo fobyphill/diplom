80e2ded8b064f8b135e66d472aadb4176286e3b8
Philipp <www.phill.999@gmail.com>
2020-04-30 06:17:12 -0300

    add page bils

---
+++ temp_web1/bils.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace temp_web1
+{
+    public partial class bils : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void b_add_con_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void b_change_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void b_delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void b_yes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void b_no_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
